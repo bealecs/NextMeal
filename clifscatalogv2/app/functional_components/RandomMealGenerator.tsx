c4d1b19da253ec162fe70b1e055d39356e860e17
bealecs <cliftonscottbeale@gmail.com>
2023-07-08 20:56:23 -0500

    destructured further from response obj

--- clifscatalogv2/app/functional_components/RandomMealGenerator.tsx
+++ clifscatalogv2/app/functional_components/RandomMealGenerator.tsx
@@ -1,30 +1,33 @@
 import Image from "next/image";
 
-//Leverages Spoonaculars random recipe API endpoint 
+//Leverages Spoonaculars random recipe API endpoint
 async function getRandomMeal() {
-    const res = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.SPOONACULAR_API_KEY}`);
-    
-    if(!res.ok) {
-        throw new Error("Oops... I wasn't able to grab a meal for you... Please try again");
-    }
+  const res = await fetch(
+    `https://api.spoonacular.com/recipes/random?apiKey=${process.env.SPOONACULAR_API_KEY}`,
+    { next: { revalidate: 300 } }
+  ); //revalidates cache every 5 mins (300s)
 
-    return res.json();
+  if (!res.ok) {
+    throw new Error(
+      "Oops... I wasn't able to grab a meal for you... Please try again"
+    );
+  }
+
+  return res.json();
 }
 
 //This is going to be the component for the random meal generator
 export default async function RandomMealGenerator() {
-    const data = await getRandomMeal();
-    
-    return (    
-        data.recipes.map((recipe) => {
-            const { name, originalName, image, id, sourceUrl } = recipe;
-    
-            return (
-                <section key={id}>
-                    <h3>{sourceUrl}</h3>
-                    <Image src={image} width={100} height={100} alt={originalName} />
-                </section>
-            )
-        })
-    )
+  const data = await getRandomMeal();
+
+  return data.recipes.map((recipe) => {
+  const { image, id, title, summary, /*spoonacularSourceUrl*/ } = recipe;
+
+    return (
+      <section key={id}>
+        <h3>{title}</h3>
+        <Image src={image} width={200} height={200} alt={summary} />
+      </section>
+    );
+  });
 }
