a44d2ef14b8565c37bb4daab5f98111d9d6d9721
bealecs <cliftonscottbeale@gmail.com>
2023-07-10 16:16:07 -0500

    made changes to layout for chat component

--- clifscatalogv2/app/components/HeroSection.tsx
+++ clifscatalogv2/app/components/HeroSection.tsx
@@ -1,13 +1,29 @@
+"use client";
+import Image from "next/image";
 import Chat from "../functional_components/Chat";
-import RandomMealGenerator from "../functional_components/RandomMealGenerator";
 import "../globalStyles.css";
 import HeroSectionStyles from "../modular_css/HeroSection.module.css";
+import { useState } from "react";
 
 export const HeroSection = () => {
+  const [showChat, setShowChat] = useState(false);
+
+  const handleClick = () => {
+    setShowChat(!showChat);
+  };
+
   return (
     <main className={HeroSectionStyles.container}>
       {/* set a conditional here for whether the user is logged in or not for the hero message */}
-      <Chat />
-    </main>    
+      <Image
+        src="/chef_kiss.svg"
+        width={100}
+        height={100}
+        alt="something"
+        onClick={handleClick}
+        className={HeroSectionStyles.chatbotImage}
+      />
+      {showChat && <Chat />}
+    </main>
   );
 };
