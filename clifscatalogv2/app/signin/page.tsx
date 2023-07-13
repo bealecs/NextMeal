72c7e794cce1fbdb45788fc9576557267c3bdc88
bealecs <cliftonscottbeale@gmail.com>
2023-07-12 22:37:51 -0500

    added background to sign in and sign up components

--- clifscatalogv2/app/signin/page.tsx
+++ clifscatalogv2/app/signin/page.tsx
@@ -1,7 +1,9 @@
-"use client"
+"use client";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
+import "../globalStyles.css";
+import SignInStyles from "../modular_css/SignIn.module.css";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
@@ -18,33 +20,37 @@ export default function SignIn() {
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Sign In</button>
-      </form>
-      <div>
-        <p>
-          Don't have an account yet?
-          <Link href="/signup">Sign up now</Link>
-        </p>
+    <section className={SignInStyles.container}>
+      <Link href="/" className={SignInStyles.back}>← Back to home</Link>
+      <div className={SignInStyles.formDiv}>
+        <h3>Sign in below</h3>
+        <form onSubmit={handleSubmit} className={SignInStyles.form}>
+          <label>
+            Email
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Sign In</button>
+        </form>
+        <div className={SignInStyles.alternate}>
+          <p>
+            Don't have an account yet?
+            <Link href="/signup">Sign up now</Link>
+          </p>
+        </div>
       </div>
     </section>
   );
