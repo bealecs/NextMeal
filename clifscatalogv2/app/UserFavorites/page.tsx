a83c4c2cebda0c1dc88340d54e16309ec42c0ced
bealecs <cliftonscottbeale@gmail.com>
2023-07-17 14:12:26 -0500

    some changes

--- clifscatalogv2/app/UserFavorites/page.tsx
+++ clifscatalogv2/app/UserFavorites/page.tsx
@@ -1,9 +1,29 @@
+"use client"
+import { getSession } from "next-auth/react";
 import React from "react"
 
-export default function UserFavorites() {
+interface UserFavorites {
 
+}
+
+async function getUserFavorites() {
+    const session = await getSession();
+    const favoritesResponse = await fetch(`/api/user/[${session.user.id}]/route.tsx`);
+    
+    if(!favoritesResponse.ok) {
+        //This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch your current favorite recipes.')
+    }
+
+    return favoritesResponse.json();
+}
+
+export async function UserFavorites() {
+    const favorites = await getUserFavorites();
+    
     return (
         <section>
+            {/* {favorites && } */}
             <p>User Favorites here</p>
         </section>
     )
