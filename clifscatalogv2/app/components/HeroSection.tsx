fa9b4055d5399f02ba18545ea2fbeb692d6b8c16
creatorjv <jvdevelops@gmail.com>
2023-08-03 13:35:09 -0400

    removed unnecessary imports and code

--- clifscatalogv2/app/components/HeroSection.tsx
+++ clifscatalogv2/app/components/HeroSection.tsx
@@ -1,36 +1,28 @@
-"use client";
-import Image from "next/image";
-import Chat from "../functional_components/Chat";
-import "../globalStyles.css";
 import HeroSectionStyles from "../modular_css/HeroSection.module.css";
-import { Suspense, useState } from "react";
 import { Session } from "next-auth";
 import SignIn from "../signin/page";
 import { signOut } from "next-auth/react";
 import UserFavoritesDisplay from "../functional_components/UserFavoritesDisplay";
 import SousChef from "../functional_components/SousChef";
+import "../globalStyles.css";
 
 interface Props {
   session: Session;
 }
 
 export const HeroSection = (props: Props) => {
-
   return (
     <main className={HeroSectionStyles.container}>
       {props.session ? (
         <div>
           <h2>Welcome, {props.session.user.name}</h2>
-          {/* <Suspense> */}
-
           <UserFavoritesDisplay session={props.session} />
-          {/* </Suspense> */}
           <button onClick={() => signOut()}>Sign out</button>
         </div>
       ) : (
         <SignIn />
       )}
-      <SousChef/>
+      <SousChef />
     </main>
   );
 };
