7085982c134f0637ec8e189a0f7c04996346b73f
bealecs <cliftonscottbeale@gmail.com>
2023-08-11 17:02:48 -0500

    add func for posting user preferences to DB

---
+++ clifscatalogv2/app/api/user/preferences/route.tsx
@@ -0,0 +1,38 @@
+import prisma from "@/app/lib/prisma";
+
+interface RequestBody {
+    checked: {
+      theme: boolean;
+      noDairy: boolean;
+      nutAllergy: boolean;
+      fishAllergy: boolean;
+      vegan: boolean;
+      vegetarian: boolean;
+      noRedMeat: boolean;
+      noPork: boolean;
+      dieting: boolean;
+    }
+    userId: number;
+  }
+  
+  export async function POST(request: Request) {
+    const body: RequestBody = await request.json();
+ //dont forget to pass the session's user id into this as well to tie the preferences to the specific user
+    const newPreferences = await prisma.preferences.create({
+        data: {
+            theme: body.checked.theme,
+            noDairy: body.checked.noDairy,
+            nutAllergy: body.checked.nutAllergy,
+            fishAllergy: body.checked.fishAllergy,
+            vegan: body.checked.vegan,
+            vegetarian: body.checked.vegetarian,
+            noRedMeat: body.checked.noRedMeat,
+            noPork: body.checked.noPork,
+            dieting: body.checked.dieting,
+            userId: body.userId,
+        },
+      });
+      
+      const {...result} = newPreferences;
+    return new Response(JSON.stringify(result));
+  }
--- clifscatalogv2/prisma/migrations/20230811215610_init/migration.sql
+++ clifscatalogv2/prisma/migrations/20230811215610_init/migration.sql
@@ -19,8 +19,28 @@ CREATE TABLE "Favorites" (
     CONSTRAINT "Favorites_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Preferences" (
+    "id" SERIAL NOT NULL,
+    "theme" BOOLEAN NOT NULL,
+    "noDairy" BOOLEAN NOT NULL,
+    "nutAllergy" BOOLEAN NOT NULL,
+    "fishAllergy" BOOLEAN NOT NULL,
+    "vegan" BOOLEAN NOT NULL,
+    "vegetarian" BOOLEAN NOT NULL,
+    "noRedMeat" BOOLEAN NOT NULL,
+    "noPork" BOOLEAN NOT NULL,
+    "dieting" BOOLEAN NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Preferences_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Favorites" ADD CONSTRAINT "Favorites_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Preferences" ADD CONSTRAINT "Preferences_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
