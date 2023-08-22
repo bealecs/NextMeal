20065a0f311c076205eb33f08ddcf0ad6249c054
bealecs <cliftonscottbeale@gmail.com>
2023-08-21 22:38:29 -0500

    added interfaces for destructured data return

--- clifscatalogv2/app/functional_components/FullMealInfo.tsx
+++ clifscatalogv2/app/functional_components/FullMealInfo.tsx
@@ -22,6 +22,42 @@ export const FullMealInfo = async (props: Props) => {
             mealData.map((meal) => {
                 //add logic here to destructure the mapped meal item
                 //then, provide jsx using destructured values to build full meal info page
+                interface ExtendedIngredients {
+                    amount: number;
+                    id: number;
+                    image: string;
+                    original: string;
+                  }
+                interface WinePairing {
+                    pairedWines: string[];
+                }
+                //Interface for all data that will be available in the FullMealInfo component
+                interface DestructuredMealInfo {
+                    id: number;
+                    title: string;
+                    image: string;
+                    servings: number;
+                    readyInMinutes: number;
+                    dairyFree: boolean;
+                    glutenFree: boolean;
+                    instructions: string;
+                    vegan: boolean;
+                    vegetarian: boolean;
+                    veryHealthy: boolean;
+                    veryPopular: boolean;
+                    weighWatcherSmartPoints: number;
+                    dishTypes: string[];
+                    extendedIngredients: ExtendedIngredients[]; //This will be an array of all of the ingriedients. Each index in the array has the data from extendedIngredients interface
+                    summary: string;
+                    winePairing: WinePairing;
+                    pairingText: string;
+                }
+
+                const destructuredMealInfo: DestructuredMealInfo = meal;
+
+                return (
+                    <></>
+                )
             })}
         </>
     )}
