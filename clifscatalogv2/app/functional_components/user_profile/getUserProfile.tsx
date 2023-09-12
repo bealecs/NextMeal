4ff1a0aa019409d9d80145c740394e832e4eee66
bealecs <cliftonscottbeale@gmail.com>
2023-09-12 11:21:49 -0500

    more and more hotfixes kept coming. New branch

--- clifscatalogv2/app/functional_components/user_profile/getUserProfile.tsx
+++ clifscatalogv2/app/functional_components/user_profile/getUserProfile.tsx
@@ -1,6 +1,6 @@
 export async function getUserProfile(userId: number, accessToken: string) {
   //soon, make this call to database url env variable so we dont have to keep swapping the url between dev and prod
-    const favoritesResponse = await fetch(`https://next-meal-cookbook.vercel.app/api/user/${userId}`, {
+    const favoritesResponse = await fetch(`http://localhost:3000/api/user/${userId}`, {
       headers: {
         "Authorization": `${accessToken}`,
       },
