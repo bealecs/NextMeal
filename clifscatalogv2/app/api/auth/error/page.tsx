63652c68daba31848b90391b9ed78e0ae1388ba7
bealecs <cliftonscottbeale@gmail.com>
2023-09-29 13:02:06 -0500

    final improvements to the failed signin page

--- clifscatalogv2/app/api/auth/error/page.tsx
+++ clifscatalogv2/app/api/auth/error/page.tsx
@@ -4,8 +4,8 @@ export default function Error() {
   
   return (
     <section style={{height:'100vh', textAlign:'center',width:'100%',margin:'auto'}}>
-      <h4>Something went wrong with your sign in attempt</h4>
-      <p>Please ensure you have typed the correct username & password</p>
+      <h4 style={{fontSize:'2rem'}}>Something went wrong with your sign in attempt</h4>
+      <p style={{fontSize:'1.5rem'}}>Please ensure you have typed the correct username & password</p>
       <br />
       <Link href="/signin">Attempt sign in again</Link>
       <p>Do not have an account? Click <a href="/signup">here</a></p>
