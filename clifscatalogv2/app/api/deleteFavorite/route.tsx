82e163659b5b8a61aa9ca64291d71a0d28e289b8
bealecs <cliftonscottbeale@gmail.com>
2024-12-03 11:11:27 -0600

    fixed deleteFavorite function

--- clifscatalogv2/app/api/deleteFavorite/route.tsx
+++ clifscatalogv2/app/api/deleteFavorite/route.tsx
@@ -2,14 +2,18 @@ import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
 interface RequestBody {
-    id: number;
+  id: number;
 }
 export async function POST(request: Request) {
+  try {
     const body: RequestBody = await request.json();
     const deleteFavorite = await prisma.favorites.delete({
-        where: {
-          id: body.id,
-        },
-    })
-    return NextResponse;
+      where: {
+        id: body.id,
+      },
+    });
+    return NextResponse.json({ success: true, data: deleteFavorite });
+  } catch (error) {
+    return NextResponse.json({ success: false, error: error }, { status: 500 });
+  }
 }
